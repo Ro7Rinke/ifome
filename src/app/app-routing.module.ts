8ce1ec785b7aefdc0432a40a26e93b58c143f33b
JairoDoni <jairo.doni97@gmail.com>
2019-11-11 01:19:45 -0300

    Layout das Telas de Login e de Cadastro completo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},  { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },  { path: 'cadastro', loadChildren: './pages/cadastro/cadastro.module#CadastroPageModule' },
+
 
 ];
 
---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  constructor(){}
+  ngOnInit() { }
+
+  
+}
