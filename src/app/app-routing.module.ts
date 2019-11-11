6f2d2ceacf4d073656ea0d6aa83263a4bd8db033
JairoDoni <jairo.doni97@gmail.com>
2019-11-11 03:01:28 -0300

    Criei a pagina de Comercios e mexi só um pouco no layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'comercios', pathMatch: 'full' },
   { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
-  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },  { path: 'cadastro', loadChildren: './pages/cadastro/cadastro.module#CadastroPageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'cadastro', loadChildren: './pages/cadastro/cadastro.module#CadastroPageModule' },
+  { path: 'comercios', loadChildren: './pages/comercios/comercios.module#ComerciosPageModule' },
 
 
 ];
